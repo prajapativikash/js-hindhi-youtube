58496761650cb7a6e895294b75513d9888ef85a0
vikash prajapati <134732674+prajapativikash@users.noreply.github.com>
2024-03-12 19:02:04 +0000

    01_vaiable is done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const name = 'vikash'
+let accountId = 1234
+var AccountPassword = 2344;
+city = "luknow";
+let age;
+console.table([accountId,AccountPassword,city,age])
+
+// name = "akash"  this is not allowed
